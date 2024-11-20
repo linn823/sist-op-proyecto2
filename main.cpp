03bdeee6277c1f586738ab3d0094e6ec4f36e07a
Xeraclom14 <67525051+linn823@users.noreply.github.com>
2024-11-19 23:38:28 -0300

    añadido logger a main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <mutex>
 #include "Monitor.h"
 #include "CircularQueue.h"
-#include "Logger.h"
 
 using namespace std;
+
+ofstream logFile;
+mutex logMutex;
+
+void iniciarLog(const string &filename) {
+    logFile.open(filename, ios::out | ios::app);
+    if (!logFile) {
+        cerr << "Error: Unable to open log file" << endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+void log(const string &message) {
+    lock_guard<mutex> lock(logMutex);
+    if (logFile.is_open()) {
+        logFile << message << endl;
+    }
+}
+
+void cerrarLog() {
+    if (logFile.is_open()) {
+        logFile.close();
+    }
+}
 
 void parseArguments(int argc, char *argv[], int &producers, int &consumers, int &initQueueSize, int &maxWaitTime) {
     int opt;
@@ -15,51 +39,57 @@ void parseArguments(int argc, char *argv[], int &producers, int &consumers, int 
             case 'c': consumers = stoi(optarg); break;
             case 's': initQueueSize = stoi(optarg); break;
             case 't': maxWaitTime = stoi(optarg); break;
-            default: cerr << "Invalid arguments!\n"; exit(EXIT_FAILURE);
+            default: cerr << "Error en los argumentos\n"; exit(1);
         }
     }
 }
 
-void producerFunction(CircularQueue &queue, int id, Logger &logger) {
+void producerFunction(CircularQueue &queue, int id) {
     for (int i = 0; i < 10; ++i) {
         queue.enqueue(i);
-        logger.log("Producer " + to_string(id) + " added " + to_string(i));
+        log("Productor " + to_string(id) + " añadio " + to_string(i));
     }
 }
 
-void consumerFunction(CircularQueue &queue, int id, Logger &logger, int maxWaitTime) {
+void consumerFunction(CircularQueue &queue, int id, int maxWaitTime) {
     while (true) {
         try {
             int value = queue.dequeue();
-            logger.log("Consumer " + to_string(id) + " removed " + to_string(value));
+            log("Consumidor " + to_string(id) + " quito " + to_string(value));
         } catch (const runtime_error &) {
-            // Handle max wait time if needed
+            
             break;
         }
     }
 }
 
 int main(int argc, char *argv[]) {
-    // Parse command-line arguments
     int producers = 0, consumers = 0, initQueueSize = 0, maxWaitTime = 0;
     parseArguments(argc, argv, producers, consumers, initQueueSize, maxWaitTime);
     
     Monitor monitor;
     CircularQueue queue(initQueueSize, monitor);
-    Logger logger("queue_log.txt");
+    iniciarLog("queue_log.txt");
 
     vector<thread> producerThreads, consumerThreads;
 
     for (int i = 0; i < producers; ++i) {
-        producerThreads.emplace_back(producerFunction, ref(queue), i, ref(logger));
+        producerThreads.emplace_back(producerFunction, ref(queue), i);
     }
 
     for (int i = 0; i < consumers; ++i) {
-        consumerThreads.emplace_back(consumerFunction, ref(queue), i, ref(logger), maxWaitTime);
+        consumerThreads.emplace_back(consumerFunction, ref(queue), i, maxWaitTime);
     }
 
-    for (auto &t : producerThreads) t.join();
-    for (auto &t : consumerThreads) t.join();
+    for (size_t i = 0; i < producerThreads.size(); ++i) {
+        producerThreads[i].join();
+    }
+
+    for (size_t i = 0; i < consumerThreads.size(); ++i) {
+        consumerThreads[i].join();
+    }
+
+    cerrarLog();
 
     return 0;
 }
